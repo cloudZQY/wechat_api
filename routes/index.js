3b7a4b160baef668758d2124acc21c49554f84b2
cloudzqy <498219667@qq.com>
2018-04-24 17:28:36 +0800

    post

--- routes/index.js
+++ routes/index.js
@@ -8,39 +8,42 @@ var crypto = require("crypto");
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/wx', validateToken);
+router.post('/wx', function(req, res, next) {
+  console.log(req.body);
+  res.send('success');
+});
 
-function sha1(str){
-  var md5sum = crypto.createHash("sha1");
-  md5sum.update(str);
-  str = md5sum.digest("hex");
-  return str;
-}
+// function sha1(str){
+//   var md5sum = crypto.createHash("sha1");
+//   md5sum.update(str);
+//   str = md5sum.digest("hex");
+//   return str;
+// }
 
-function validateToken(req,res){
-  var query = url.parse(req.url,true).query;
-  //console.log("*** URL:" + req.url);
-  //console.log(query);
-  var signature = query.signature;
-  var echostr = query.echostr;
-  var timestamp = query['timestamp'];
-  var nonce = query.nonce;
-  var oriArray = new Array();
-  oriArray[0] = nonce;
-  oriArray[1] = timestamp;
-  oriArray[2] = "hello";//这里是你在微信开发者中心页面里填的token，而不是****
-  oriArray.sort();
-  var original = oriArray.join('');
-  console.log("Original str : " + original);
-  console.log("Signature : " + signature );
-  var scyptoString = sha1(original);
-  if(signature == scyptoString){
-    res.send(echostr);
-    console.log("Confirm and send echo back");
-  }else {
-    res.send("false");
-    console.log("Failed!");
-  }
-}
+// function validateToken(req,res){
+//   var query = url.parse(req.url,true).query;
+//   //console.log("*** URL:" + req.url);
+//   //console.log(query);
+//   var signature = query.signature;
+//   var echostr = query.echostr;
+//   var timestamp = query['timestamp'];
+//   var nonce = query.nonce;
+//   var oriArray = new Array();
+//   oriArray[0] = nonce;
+//   oriArray[1] = timestamp;
+//   oriArray[2] = "hello";//这里是你在微信开发者中心页面里填的token，而不是****
+//   oriArray.sort();
+//   var original = oriArray.join('');
+//   console.log("Original str : " + original);
+//   console.log("Signature : " + signature );
+//   var scyptoString = sha1(original);
+//   if(signature == scyptoString){
+//     res.send(echostr);
+//     console.log("Confirm and send echo back");
+//   }else {
+//     res.send("false");
+//     console.log("Failed!");
+//   }
+// }
 
 module.exports = router;
